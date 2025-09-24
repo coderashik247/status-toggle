25ef991700b22cd7ef78c1d9064c869f895e67a4
Ashik <ashikurashik20999@gmail.com>
2025-09-25 00:44:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer/Footer"
+import Navbar from "./Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
